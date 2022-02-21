1a60faba34a8eba27796e7a38a1eacb2747ec930
Harvinraj <harvinraj@gmail.com>
2022-02-21 16:17:24 +0800

    No changes

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ const App = () => {
 
   useEffect(() => {
     axios
-
       .get(`https://api.coingecko.com/api/v3/search/trending`)
       .then((res) => {
         console.log(res.data.coins);
